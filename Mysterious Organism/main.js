05fb1894433021c46521c8462f2fb0007f496591
DariaRaiRai <dari.shamrai@gmail.com>
2022-10-10 10:33:13 +0200

    update objects mutations

--- Mysterious Organism/main.js
+++ Mysterious Organism/main.js
@@ -12,3 +12,61 @@ const mockUpStrand = () => {
   }
   return newStrand;
 };
+
+// Factory function to model pAequor specimens
+const pAequorFactory = (number, array) => {
+  return {
+    specimen: number,
+    dna: array,
+
+    // mutates a base randomly
+    mutate() {
+      const base = Math.floor(Math.random() * 15);
+      prev = array[base];
+      do {
+        array[base] = returnRandBase();
+      } while (array[base] === prev);
+    },
+
+    // rturns similarty (%) between two specimen
+    compareDNA(pAequor) {
+      same = [];
+      this.dna.forEach((base, index) => {
+        if (base === pAequor[index]) {
+          same.push(base);
+          console.log(base);
+        }
+      });
+      return `speciman #1 and speciman #2 have ${
+        (same.length / this.dna.length) * 100
+      }% DNA in common`;
+    },
+
+    // return true if C and G bases make up at least 60%
+    willLikelySurvive() {
+      count = 0;
+      this.dna.forEach((base) => {
+        if (base === "C" || base === "G") {
+          count += 1;
+        }
+      });
+      return count / this.dna.length >= 0.6;
+    },
+  };
+};
+
+// return array of 30 random specimens likely to survive
+const thirtySurvivors = () => {
+  let survivors = [];
+  let i = 1;
+  while (i < 31) {
+    instance = pAequorFactory(i, mockUpStrand());
+    if (instance.willLikelySurvive()) {
+      survivors.push([`Specimen: ${instance.specimen}, DNA: ${instance.dna}`]);
+      i++;
+    }
+  }
+  return survivors;
+};
+
+console.log(thirtySurvivors());
