c90cee4ec981ed3ed197d19fbd4612f192196bdd
DariaRaiRai <dari.shamrai@gmail.com>
2022-11-29 09:53:25 +0100

    add Book class
    add constructor with arguments to be used to set properties that do not have default values. Call super on the first constructor method. Set other props. Add getters to inherit Media's props.

--- Build A Library/main.js
+++ Build A Library/main.js
@@ -34,3 +34,19 @@ class Media {
     this.ratings.push(value);
   }
 }
+
+class Book extends Media {
+  constructor(author, title, pages) {
+    super(title);
+    this._author = author;
+    this._page = pages;
+  }
+
+  get author() {
+    return this._author;
+  }
+
+  get pages() {
+    return this._pages;
+  }
+}
